0c84b5de050a5ba8a5dad42cf47dfcc89721a24a
Ian Kenji <janusz7z@pm.me>
2022-12-01 13:43:43 +0100

    updated download_task.sh

--- download_task.sh
+++ download_task.sh
@@ -6,6 +6,6 @@ url="https://adventofcode.com/2022/day/$1"
 session=$(cat ./session.txt)
 mkdir -p ./$day
 
-curl --output ./$day/task.html $url
+curl --cookie $session --output ./$day/task.html $url
 curl --cookie $session --output ./$day/input.txt $url/input
 
